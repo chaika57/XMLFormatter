7fca66cb1641b514dd121da08b5db8e89cbe0f4d
chaika57 <chaika57@gmail.com>
2015-11-26 14:43:01 +0200

    commit #1

---
+++ XMLFormater/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace XMLFormater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createXMLButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // createXMLButton
+            // 
+            this.createXMLButton.Enabled = false;
+            this.createXMLButton.Location = new System.Drawing.Point(25, 61);
+            this.createXMLButton.Name = "createXMLButton";
+            this.createXMLButton.Size = new System.Drawing.Size(109, 23);
+            this.createXMLButton.TabIndex = 0;
+            this.createXMLButton.Text = "Create XML";
+            this.createXMLButton.UseVisualStyleBackColor = true;
+            this.createXMLButton.Click += new System.EventHandler(this.ctreateXMLButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(25, 23);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(109, 23);
+            this.uploadButton.TabIndex = 1;
+            this.uploadButton.Text = "Upload Excel";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(154, 28);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.fileNameLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 110);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.createXMLButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createXMLButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Label fileNameLabel;
+    }
+}
+
---
+++ XMLFormater/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace XMLFormater
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateXML()
+        {
+            int index = 0; //DataTable row index
+            int tableNumberRows = ds.Tables[0].Rows.Count; //counts number of rows in DataTable
+                        
+            CreditTransferTransactionInformation10[] crdtTransTransacInfoTransactions = new CreditTransferTransactionInformation10[tableNumberRows];            
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {                
+                CreditTransferTransactionInformation10 crdtTransTransacInfo = new CreditTransferTransactionInformation10();
+                
+                PaymentIdentification1 paymentIndNum = new PaymentIdentification1();
+                paymentIndNum.EndToEndId = index.ToString();
+
+                EquivalentAmount2 eqvAmount = new EquivalentAmount2();
+                ActiveOrHistoricCurrencyAndAmount curAndAmnt = new ActiveOrHistoricCurrencyAndAmount();
+                curAndAmnt.Ccy = row[10].ToString();//Currency
+                curAndAmnt.Value = decimal.Parse(row[9].ToString());//Transaction ammount
+
+                eqvAmount.Amt = curAndAmnt;
+
+                AmountType3Choice amntType = new AmountType3Choice();
+                amntType.Item = eqvAmount;
+
+                crdtTransTransacInfo.PmtId = paymentIndNum;
+                crdtTransTransacInfo.Amt = amntType;
+
+                crdtTransTransacInfoTransactions[index] = crdtTransTransacInfo;              
+
+                index++;
+            }
+
+            PaymentInstructionInformation3[] pmntInstrInf = new PaymentInstructionInformation3[1];
+            PaymentInstructionInformation3 pmntInstrInfIteam = new PaymentInstructionInformation3();
+            pmntInstrInfIteam.CdtTrfTxInf = crdtTransTransacInfoTransactions;
+            pmntInstrInf[0] = pmntInstrInfIteam;
+
+            Authorisation1Choice[] authChoiceArray = new Authorisation1Choice[1];
+            Authorisation1Choice authChoice = new Authorisation1Choice();
+            authChoice.Item = "Sergei";
+            authChoiceArray[0] = authChoice;
+
+            GroupHeader32 grpHdr = new GroupHeader32();
+            grpHdr.Authstn = authChoiceArray;
+
+            CustomerCreditTransferInitiationV03 cctiv = new CustomerCreditTransferInitiationV03();
+            cctiv.GrpHdr = grpHdr;
+            cctiv.PmtInf = pmntInstrInf;
+
+            Document document = new Document();
+         
+            document.CstmrCdtTrfInitn = cctiv;          
+
+            var data = document;
+            var serializer = new XmlSerializer(typeof(Document));
+            using (var stream = new StreamWriter("C:\\Users\\yulya\\Desktop\\test.xml"))               
+               
+           serializer.Serialize(stream, data);
+
+        }
+
+
+        private void ctreateXMLButton_Click(object sender, EventArgs e)
+        {
+            CreateXML();
+        }
+
+        private void UploadExcel(string fileName)
+        {           
+            try
+            {
+                OleDbConnection MyConnection;                
+                OleDbDataAdapter MyCommand;
+
+                try
+                {
+                    MyConnection = new OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties=Excel 8.0;");
+                }
+                catch
+                {
+                    MyConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=Excel 12.0;");
+                }
+
+                MyConnection.Open();
+                DataTable Sheets = MyConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+               
+                string worksheets = Sheets.Rows[0]["TABLE_NAME"].ToString();
+
+                MyCommand = new OleDbDataAdapter("select * from [" + worksheets + "]", MyConnection);
+                MyCommand.TableMappings.Add("Table", "TestTable");
+                ds = new System.Data.DataSet();
+                MyCommand.Fill(ds);                
+                MyConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }                
+
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Filter = "Excel Files | *.xls"; 
+            dialog.Multiselect = false;
+            try
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string path = dialog.FileName;
+                    UploadExcel(path);
+
+                    fileNameLabel.Text = Path.GetFileName(path);//display file name when file is uploaded
+                    createXMLButton.Enabled = true;//activate upload button
+                    
+                }
+            }
+            catch { }
+        }
+    }
+}
